ac0b61f2e1de7c2559fd14fa0d1faea8e17addd6
Roman <roman.kulbacki@gmail.com>
2022-02-15 11:03:52 +0100

    signin ok

--- src/app/common/services/user.service.ts
+++ src/app/common/services/user.service.ts
@@ -15,40 +15,40 @@ export class UserService {
 
   constructor(private http: HttpClient) { }
 
-  httpHeaders = new HttpHeaders()
-    .set('content-type', 'application/json')
-    .set('Access-Control-Allow-Origin', '*')
+  // httpHeaders = new HttpHeaders()
+  //   .set('content-type', 'application/json')
+  //   .set('Access-Control-Allow-Origin', '*')
 
 
     _searchGamerById$(id: string): Observable<Gamer> {
       var uri = "user/gamer/" + id;
-      return this.http.get<Gamer>(`${this.ROOT_URL}/${uri}`);
+      return this.http.get<Gamer>(uri);
     }
 
     addGamer$(gamer: Gamer): Observable<Gamer> {
       var uri = "user/gamer/save";
-      return this.http.post<Gamer>(`${this.ROOT_URL}/${uri}`, gamer);
+      return this.http.post<Gamer>(uri, gamer);
       //,httpOptions)
     }
 
     _patchGamerById$(id: string, password: PasswordDto): Observable<any> {
       var uri = "user/gamer/" + id + "/patchPassword";
-      return this.http.put<any>(`${this.ROOT_URL}/${uri}`, password);
+      return this.http.put<any>(uri, password);
     }
 
     _searchModeratorById$(id: string): Observable<Moderator> {
       var uri = "user/moderator/" + id;
-      return this.http.get<Moderator>(`${this.ROOT_URL}/${uri}`);
+      return this.http.get<Moderator>(uri);
     }
 
     addModerator$(moderator: Moderator): Observable<Moderator> {
       var uri = "user/moderator/save";
-      return this.http.post<Moderator>(`${this.ROOT_URL}/${uri}`, moderator);
+      return this.http.post<Moderator>(uri, moderator);
       //,httpOptions)
     }
 
     _patchModeratorById$(id: string, password: PasswordDto): Observable<any> {
       var uri = "user/moderator/" + id + "/patchPassword";
-      return this.http.put<any>(`${this.ROOT_URL}/${uri}`, password);
+      return this.http.put<any>(uri, password);
     }
 }
--- src/app/connexion/sign-in/sign-in.component.ts
+++ src/app/connexion/sign-in/sign-in.component.ts
@@ -1,9 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormControl, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 
 import { Gamer } from 'src/app/common/model/gamer';
 import { LoginDTO } from 'src/app/common/model/login-dto';
 import { AuthenticationService } from 'src/app/common/services/authentication.service';
+import { TokenStorageService } from 'src/app/common/services/token-storage.service';
 
 @Component({
   selector: 'app-sign-in',
@@ -12,7 +14,10 @@ import { AuthenticationService } from 'src/app/common/services/authentication.se
 })
 export class SignInComponent implements OnInit {
 
-  constructor(private authenticationService: AuthenticationService, private fb: FormBuilder) { }
+  constructor(private authenticationService: AuthenticationService,
+    private fb: FormBuilder,
+    private router: Router,
+    private tokenStorageService: TokenStorageService) { }
 
   login: LoginDTO = new LoginDTO();
   profileForm = this.fb.group({
@@ -26,6 +31,14 @@ export class SignInComponent implements OnInit {
   errorMessage = '';
 
   ngOnInit(): void {
+    this.tokenStorageService.estConnecte.subscribe(isConnect => {
+      // this.isLoggedIn = !!this.tokenStorage.getToken();
+      this.isLoggedIn = isConnect;
+      this.gamer = this.tokenStorageService.getUser();
+      if (!isConnect) {
+        this.refreshform();
+      }
+    });
   }
 
   get pseudoForm(): FormControl {
@@ -35,25 +48,39 @@ export class SignInComponent implements OnInit {
     return this.profileForm.get('password') as FormControl;
   }
 
+  private ignoreError = (error: Error): void => {
+    this.errorMessage = "êtes-vous inscrit ?";
+    this.isLoginFailed = true;
+  };
+  
+  private loadGamer = (data: Gamer): void => {
+    this.gamer = data;
+    // this.tokenStorageService.saveToken(data.token);
+    this.tokenStorageService.saveUser(data);
+    console.log(this.gamer);
+    this.isLoginFailed = false;
+    this.isLoggedIn = true;
+  }
+
   formSending() {
     this.login.pseudo = this.pseudoForm.value;
     this.login.password = this.passwordForm.value;
 
     this.authenticationService.signin$(this.login.pseudo, this.login.password)
-      .subscribe(
-        data => {
-          console.log(data.id);
-          // this.tokenStorage.saveToken(data.token);
-          // this.tokenStorage.saveUser(data);
-          this.gamer = data;
-          console.log(this.gamer);
-          this.isLoginFailed = false;
-          this.isLoggedIn = true;
-        },
-        (err: { error: { message: string; }; }) => {
-          this.errorMessage = "êtes-vous inscrit ?";
-          this.isLoginFailed = true;
-        }
-      );
+      .subscribe(({ next: this.loadGamer, error: this.ignoreError }))
   }
+
+  onLogout(): void {
+    this.tokenStorageService.signOut();
+    this.isLoggedIn = false;
+    this.refreshform();
+  }
+
+  refreshform() {
+    const pseudo = '';
+    const password = '';
+    this.profileForm.get('pseudo')?.setValue(pseudo);
+    this.profileForm.get('password')?.setValue(password);
+  }
+
 }
