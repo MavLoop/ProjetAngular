0d8865e77fead84b9bdd7f04a95ff591d4e97109
jurufola <moulaye.haidara@laposte.net>
2022-02-15 17:56:41 +0100

    Renaming of interface Game to GameDto

---
+++ src/app/common/model/gameDto.model.ts
@@ -0,0 +1,13 @@
+
+export interface GameDto {
+    id: number;
+    name: string;
+    description: string;
+    releaseDate: Date;
+    classificationName: string;
+    genreName: string;
+    editorName: string;
+    platformNames: string[];
+    businessModelNames: string
+
+}
