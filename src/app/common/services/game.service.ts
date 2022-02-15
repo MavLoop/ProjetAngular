49d46487e6838434f36ee631e089ebcdb851c7b9
jurufola <moulaye.haidara@laposte.net>
2022-02-15 22:42:37 +0100

    Structure Adding Game OK without custom validation on releaseDate

--- src/app/common/services/game.service.ts
+++ src/app/common/services/game.service.ts
@@ -2,8 +2,11 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { environment } from 'src/environments/environment';
+import { BusinessModel } from '../model/business-model.model';
+import { Classification } from '../model/classification.model';
 import { Editor } from '../model/editor.model';
 import { Genre } from '../model/genre.model';
+import { Platform } from '../model/platform.model';
 
 @Injectable({
   providedIn: 'root'
@@ -15,7 +18,22 @@ export class GameService {
   getAllGenres(): Observable<Genre[]>{
     return this.http.get<Genre[]>(`${this.ROOT_URL}/api/genres`);
   }
+
   getAllEditors(): Observable<Editor[]>{
     return this.http.get<Editor[]>(`${this.ROOT_URL}/api/editors`);
   }
+
+  getAllCassifications(): Observable<Classification[]>{
+    return this.http.get<Classification[]>(`${this.ROOT_URL}/api/classifications`);
+  }
+  
+  getAllPlatforms(): Observable<Platform[]>{
+    return this.http.get<Platform[]>(`${this.ROOT_URL}/api/platforms`);
+  }
+  getAllBusinessModels(): Observable<BusinessModel[]>{
+    return this.http.get<BusinessModel[]>(`${this.ROOT_URL}/api/businessModels`);
+  }
+
+  
+
 }
--- src/app/game/add-game/add-game.component.ts
+++ src/app/game/add-game/add-game.component.ts
@@ -1,5 +1,11 @@
 import { Component, Input, OnInit } from '@angular/core';
-import { FormBuilder, FormControl, Validators } from '@angular/forms';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { BusinessModel } from 'src/app/common/model/business-model.model';
+import { Classification } from 'src/app/common/model/classification.model';
+import { Editor } from 'src/app/common/model/editor.model';
+import { Genre } from 'src/app/common/model/genre.model';
+import { Platform } from 'src/app/common/model/platform.model';
+import { GameService } from 'src/app/common/services/game.service';
 
 @Component({
   selector: 'app-add-game',
@@ -7,14 +13,14 @@ import { FormBuilder, FormControl, Validators } from '@angular/forms';
   styleUrls: ['./add-game.component.css']
 })
 export class AddGameComponent implements OnInit {
-  editors!: string[];
-  genres!: string[];
-  classifications!: string[];
-  platforms!: string[];
-  businessModels!: string[];
+  editors!: Editor[];
+  genres!: Genre[];
+  classifications!: Classification[];
+  platforms!: Platform[];
+  businessModels!: BusinessModel[];
 
 
-  addGameForm = this.fb.group({
+  addGameForm: FormGroup = this.fb.group({
     name: ['', Validators.required],
     editorName: ['', Validators.required],
     releaseDate: [''],
@@ -25,11 +31,42 @@ export class AddGameComponent implements OnInit {
     businessModelName: ['', Validators.required]
 
   });
-  constructor(private fb: FormBuilder) { }
+  constructor(private fb: FormBuilder, private gameService: GameService) { }
 
   ngOnInit(): void {
+    this.genres = [{id: 1, name: "genre 1"}, {id: 2, name: "genre 2"}];
+    this.editors = [{id: 1, name: "editor 1"}, {id: 2, name: "editor 2"}];
+    this.classifications = [{id: 1, name: "cassification 1"}, {id: 2, name: "classification 2"}];
+    this.platforms = [{id: 1, name: "platform 1"}, {id: 2, name: "platform 2"}];
+    this.businessModels = [{id: 1, name: "businessModel 1"}, {id: 2, name: "businessModel 2"}];
+    // Gettings all the genres from database
+    /*this.gameService.getAllGenres().subscribe({
+      next: (data) => this.genres = data,
+      error: (error => console.error(error))
+    });
+    // Gettings all the editors from database
+    /*this.gameService.getAllEditors().subscribe({
+      next: (data) => this.editors = data,
+      error: (error => console.error(error))
+    });
+    // Gettings all the classifications from database
+    /*this.gameService.getAllCassifications().subscribe({
+      next: (data) => this.classifications = data,
+      error: (error => console.error(error))
+    });
+    // Gettings all the platforms from database
+    /*this.gameService.getAllPlatforms().subscribe({
+      next: (data) => this.platforms = data,
+      error: (error => console.error(error))
+    });
+    // Gettings all the businessModels from database
+    this.gameService.getAllBusinessModels().subscribe({
+      next: (data) => this.businessModels = data,
+      error: (error => console.error(error))
+    });*/
+
   }
-  
+
 
 
   get name(): FormControl {
@@ -41,7 +78,7 @@ export class AddGameComponent implements OnInit {
   get releaseDate(): FormControl {
     return this.addGameForm.get('releaseDate') as FormControl;
   }
-  get description(): FormControl {
+  get descript(): FormControl {
     return this.addGameForm.get('description') as FormControl;
   }
   get genreName(): FormControl {
@@ -54,10 +91,22 @@ export class AddGameComponent implements OnInit {
     return this.addGameForm.get('platformNames') as FormControl;
   }
   get businessModelName(): FormControl {
-    return this.addGameForm.get('businessModel') as FormControl;
+    return this.addGameForm.get('businessModelName') as FormControl;
   }
 
   addGame() {
     console.log(this.addGameForm);
+    console.log(this.findInvalidControls(this.addGameForm))
+  }
+
+  findInvalidControls(f: FormGroup) {
+    const invalid = [];
+    const controls = f.controls;
+    for (const name in controls) {
+      if (controls[name].invalid) {
+        invalid.push(name);
+      }
+    }
+    return invalid;
   }
 }
