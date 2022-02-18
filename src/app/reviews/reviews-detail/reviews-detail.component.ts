3b116479434b1ad2f2a8094cab113ba9cd5d3b77
mavlopp <alaric.smith@outlook.fr>
2022-02-18 11:25:23 +0100

    add view button to gamer list, return button working

--- src/app/reviews/reviews-detail/reviews-detail.component.ts
+++ src/app/reviews/reviews-detail/reviews-detail.component.ts
@@ -1,5 +1,7 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { ActivatedRoute, ParamMap, Router } from '@angular/router';
+import { Gamer } from 'src/app/common/model/gamer';
+import { Moderator } from 'src/app/common/model/moderator';
 import { Reviews } from 'src/app/common/model/reviews.model';
 import { ReviewsService } from 'src/app/common/services/reviews.service';
 import { TokenStorageService } from 'src/app/common/services/token-storage.service';
@@ -15,26 +17,39 @@ export class ReviewsDetailComponent implements OnInit {
 
   @Input()
   id!: string | null;
-  reviews!:Reviews;
+  reviews!: Reviews;
+  hasAdminRole = false;
+  user!: Gamer | Moderator;
 
-  constructor( private reviewsService: ReviewsService,
+  constructor(private reviewsService: ReviewsService,
     private tokenStorageService: TokenStorageService,
     private route: ActivatedRoute, private router: Router,) {
-      this.route.paramMap.subscribe((params: ParamMap) => {
-        this.id = params.get('id');
-        if(this.id !== null) {
-          this.initReviews(parseInt(this.id));
-        } else {
-          this.router.navigate(['/error-not-connecting']);
+    this.route.paramMap.subscribe((params: ParamMap) => {
+      this.id = params.get('id');
+      if (this.id !== null) {
+        this.initReviews(parseInt(this.id));
+      } else {
+        this.router.navigate(['/error-not-connecting']);
+      }
+    });
+    this.tokenStorageService.estConnecte.subscribe(isConnect => {
+      this.user = this.tokenStorageService.getUser();
+      if (isConnect) {
+        if (this.user.admin == true) {
+          this.hasAdminRole = true;
         }
-      });
-     }
+        if (this.user.admin == false) {
+          this.hasAdminRole = false;
+        }
+      }
+    });
+  }
 
   ngOnInit(): void {
   }
 
 
   initReviews(id: number): void {
-    this.reviewsService._searchReviewsById$(String(id)).subscribe((data) => {this.reviews = data});
+    this.reviewsService._searchReviewsById$(String(id)).subscribe((data) => { this.reviews = data });
   }
 }
