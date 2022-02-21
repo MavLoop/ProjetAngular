ddc59e7e2d11827cee55a67f300197eecb57ca97
mavlopp <alaric.smith@outlook.fr>
2022-02-21 13:39:16 +0100

    upload image changes

--- src/app/dashboard/dashboard-moderator/manage-game-list/upload-game-image/upload-game-image.component.ts
+++ src/app/dashboard/dashboard-moderator/manage-game-list/upload-game-image/upload-game-image.component.ts
@@ -2,6 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, ParamMap, Router } from '@angular/router';
 import { tap } from 'rxjs';
+import { Game } from 'src/app/common/model/game.model';
 import { GameService } from 'src/app/common/services/game.service';
 
 @Component({
@@ -14,12 +15,17 @@ export class UploadGameImageComponent implements OnInit {
   id!: number;
   fileName!: string;
   formData!: FormData;
+  game!: Game;
 
   constructor(private route: ActivatedRoute, private router: Router, private http: HttpClient, private gameService: GameService) {
     this.route.paramMap.subscribe((params: ParamMap) => {
       const id = params.get('id');
       if (id !== null) {
         this.id = parseInt(id);
+        this.gameService.getGameById(this.id).subscribe({
+          next: (data) => {this.game = data},
+          error: (error) => {console.log(error)}
+        })
       } else {
         this.router.navigate(['/error']);
       }
